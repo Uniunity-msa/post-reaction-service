138934c06ead00d82a2299907e3307f5865bf2a2
jiwonLee42 <ljw56b@gmail.com>
2025-05-14 20:04:40 +0900

    [CHORE] host 수정

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 // 추후 수정(테스트용)
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'post-reaction-service-db',      
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,             
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
