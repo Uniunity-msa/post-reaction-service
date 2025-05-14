8c5403a81f74eaf0801b82acf21db86638db3ee0
jiwonLee42 <ljw56b@gmail.com>
2025-05-14 19:47:51 +0900

    [CHORE] db 포트수정

--- src/config/db.js
+++ src/config/db.js
@@ -5,11 +5,11 @@ const mysql = require('mysql2');
 // 추후 수정(테스트용)
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'post-reaction-service-db',      // ✅ 컨테이너 이름 사용
-  user: process.env.DB_USER,             // .env 또는 docker 환경변수에서 주입
+  host: 'post-reaction-service-db',      
+  user: process.env.DB_USER,             
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT   
+  port: 3306
 });
 
 module.exports = { pool };
