18266d9c310742524debb170b187ecb89548a426
jiwonLee42 <ljw56b@gmail.com>
2025-05-17 11:27:33 +0900

    [CHORE] db 환경변수명 수정

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,10 @@ const mysql = require('mysql2');
 // 추후 수정(테스트용)
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,             
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.POST_SERVICE_DB_HOST,
+  user: process.env.POST_SERVICE_DB_USER,             
+  password: process.env.POST_SERVICE_DB_PASSWORD,
+  database: process.env.POST_SERVICE_DB_NAME,
   port: 3306
 });
 
