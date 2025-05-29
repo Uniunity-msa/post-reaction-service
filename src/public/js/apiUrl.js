cd0908cf540ceb491b21466cbc4fa7ebe2c55cd9
youngseoOh <dudtj2379@gmail.com>
2025-05-29 16:04:08 +0900

    [FIX] Url 수정

--- src/public/js/apiUrl.js
+++ src/public/js/apiUrl.js
@@ -1,9 +1,19 @@
-// config.js;
-export const apiUrl = 'http://34.47.84.123'; 
+const apiUrl = 'http://34.47.84.123';
 
-export const baseUrls = {
+const baseUrls = {
   post: `${apiUrl}:3000`, // post-service
   reaction: `${apiUrl}:3002`,   // post-reaction-service
   user: `${apiUrl}:3004`,     // user-service
-  start: `${apiUrl}:3001` // start-service
+  start: `${apiUrl}:3001`     // start-service
 };
+
+// Node.js 환경이면 module.exports 사용
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { baseUrls, apiUrl };
+}
+
+// 브라우저 환경이면 window에 등록
+if (typeof window !== 'undefined') {
+  window.baseUrls = baseUrls;
+  window.apiUrl = apiUrl;
+}
