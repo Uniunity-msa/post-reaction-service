043d3016f9c1be50ffd3adf1555d78b5bedfa689
hini5971 <sini5971@gmail.com>
2025-05-29 17:47:43 +0900

    chore: 스토리지 로그 추가

--- src/models/post-reactionStorage.js
+++ src/models/post-reactionStorage.js
@@ -425,6 +425,7 @@ class PostReactionStorage {
 // 게시글 존재하는지 확인
 static async validPostId(post_id) {
     try {
+        console.log("✅ baseUrls.postServiceUrl:", baseUrls.postServiceUrl);
         const response = await axios.get(`http://${baseUrls.postServiceUrl}/showPost/${post_id}`);
         console.log("post-service 통신 성공 - post 불러오기 성공: ",response);
         // 존재하면 200 OK, 데이터 포함
