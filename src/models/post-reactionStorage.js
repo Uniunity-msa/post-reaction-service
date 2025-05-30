5f41927dcc602fde2fb96502e914f0082688460b
hini5971 <sini5971@gmail.com>
2025-05-30 19:31:04 +0900

    fix: 댓글 등록 기본값 설정 추가

--- src/models/post-reactionStorage.js
+++ src/models/post-reactionStorage.js
@@ -33,10 +33,10 @@ class PostReactionStorage {
                 const formattedDateTime = getCurrentDateTime();
     
                 const insertQuery = 'INSERT INTO Comment(user_email, post_id, comment_content, comment_date) VALUES (?, ?, ?, ?)';
-    
+                
                 connection.query(
                     insertQuery,
-                    [commentInfo.user_email, commentInfo.post_id, commentInfo.comment_content, formattedDateTime],
+                    [commentInfo.user_email, commentInfo.post_id, commentInfo.comment_content, formattedDateTime, 0],
                     async (err) => {
                         connection.release();
     
